ec94919b84986bead2f8007ce0c86c9a0672b7f7
Nikolay Leipi <nikolay.semykin@googlemail.com>
2023-08-08 00:58:17 +0200

    Update nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,42 @@
+vim.opt.runtimepath:append(',~/.fzf')
+
+vim.o.sessionoptions='blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+
+vim.g.mapleader = ','
+
+vim.cmd[[colorscheme tokyonight]]
+vim.opt.number = true
+
+require('auto-session').setup {
+	auto_session_create_enabled = false
+}
+
+require('session-lens').setup {
+    path_display={'shorten'},
+}
+
+require('lualine').setup{
+  options = {
+    theme = 'tokyonight',
+  },
+  sections = {lualine_c = {require('auto-session-library').current_session_name}}
+}
+
+require('telescope').setup{
+  defaults = {
+	wrap_results = true,
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+  },
+  extensions = {
+    -- ...
+  }
+}
+
+require('lspconfig').eslint.setup{}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
